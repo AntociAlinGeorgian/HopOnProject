f1669f950eb4adbdaf8f5c4ea124102cfe3fe97d
Brinza Alin Georgian <brinza.alin.georgian@gmail.com>
2015-11-30 13:01:39 +0200

    Initial commit

---
+++ app/src/main/java/ro/proj/hopon/hoponproject/Login.java
@@ -0,0 +1,82 @@
+package ro.proj.hopon.hoponproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class Login extends Activity {
+
+    private static Button button_login;
+    private static TextView register_clickable;
+    private static TextView fgt_pass_clickable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.login_frame);
+
+
+
+        button_login = (Button) findViewById(R.id.login_BTN);
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Homepage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        register_clickable = (TextView) findViewById(R.id.register_TV);
+        register_clickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1= new Intent(getApplicationContext(),Register.class);
+                startActivity(intent1);
+            }
+        });
+
+        fgt_pass_clickable = (TextView) findViewById(R.id.fgt_pass_TV);
+        fgt_pass_clickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Forgotpass.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login_frame, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
